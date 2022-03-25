43697b7f862bac86745438803573980450f1e03a
shawnho1018 <shawnho@google.com>
2022-03-25 10:40:56 +0800

    add cicd golder

---
+++ cicd/deploy-pipeline.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+gcloud deploy apply --file cicd/clouddeploy.yaml --region asia-east1
+gcloud beta builds triggers create github \
+    --name=hello-demo \
+    --repo-name=hello-demo \
+    --repo-owner=shawnho1018 \
+    --branch-pattern=main \
+    --build-config=cicd/cloudbuild.yaml \
+    --require-approval
+
