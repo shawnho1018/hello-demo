45991c505e10f0f2877c8b4ef11c1d096230dfaf
shawnho1018 <shawnho@google.com>
2022-03-25 10:54:00 +0800

    change pipeline

--- cicd/deploy-pipeline.sh
+++ cicd/deploy-pipeline.sh
@@ -4,7 +4,7 @@ gcloud beta builds triggers create github \
     --name=hello-demo \
     --repo-name=hello-demo \
     --repo-owner=shawnho1018 \
-    --branch-pattern=main \
+    --branch-pattern="^main$" \
     --build-config=cicd/cloudbuild.yaml \
     --require-approval
 
